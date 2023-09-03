63b024931cd2cf2bfd111dc19182bc703ea47c4b
tomatoweb-net <marco@tomatoweb.net>
2023-09-03 15:21:29 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+console.log("Hello world");
+console.log(" 👩‍🚀This is my first project");
+
+let myVar: number = 3;
+myVar += 10;
+
+console.log(myVar);
+
+interface MyInterface {
+  myNumber: number;
+  myString: String;
+}
+
+let myInterface: MyInterface = {
+  myNumber: 10,
+  myString: "hello world",
+};
+
+console.log("myNumber= ", myInterface.myNumber);
+console.log("myString= ", myInterface.myString);
